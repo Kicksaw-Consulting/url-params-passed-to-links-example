dc86db2db9311f2bcc3bb85d32310e565d5f3a20
Alex Drozd <drozdster@gmail.com>
2021-08-20 16:01:23 -0500

    forwarding params to iframe.src

---
+++ paramForwarder.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // get a list of all the anchor tags (links) on this page
+    const anchorTags = document.getElementsByTagName("a");
+    // get a list of all the iframes on this page
+    const iframes = document.getElementsByTagName("iframe");
+
+    // convert current page query param string to URLSearchParams object, e.g., ?key=value => URLSearchParams.key = value
+    const pageParams = new URLSearchParams(window.location.search);
+    // convert URLSearchParams objects to regular objects, e.g., {key: "value"}
+    const pageParamsObj = paramsToObject(pageParams);
+
+    // if query params (utm params) exist
+    if (window.location.search) {
+        // decorate all the links
+        Array.from(anchorTags).forEach((a) => {
+            // make a url object out of the anchor tag's link
+            const link = new URL(a.href);
+            // update the href with the new link we've created
+            a.href = getMergedParamsUrl(link, pageParamsObj);
+        });
+        // decorate all the iframes
+        Array.from(iframes).forEach((iframe) => {
+            // make a url object out of the iframe's source
+            const link = new URL(iframe.src);
+            // update the src with the new link we've created
+            iframe.src = getMergedParamsUrl(link, pageParamsObj);
+        })
+    }
+});
+
+// convert a URLSearchParams object to a normal object
+function paramsToObject(entries) {
+    const result = {}
+    for (const [key, value] of entries) { // each 'entry' is a [key, value] tupple
+        result[key] = value;
+    }
+    return result;
+}
+
+// combines page params with params from some passed in url and returns the new url
+function getMergedParamsUrl(url, pageParamsObj) {
+    // convert the link's query param string to a URLSearchParams object
+    const linkParams = new URLSearchParams(url.search);
+    // then a regular object
+    const linkParamsObj = paramsToObject(linkParams);
+    // combine the query params from the page and the link
+    url.search = new URLSearchParams({ ...pageParamsObj, ...linkParamsObj });
+
+    return url.toString();
+}
